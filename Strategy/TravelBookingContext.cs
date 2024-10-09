20529f5344a7abac2aa85d84c011672ec12c5d0c
Malika <144811887+a-malika@users.noreply.github.com>
2024-10-09 09:09:28 +0600

    Update TravelBookingContext.cs

--- Strategy/TravelBookingContext.cs
+++ Strategy/TravelBookingContext.cs
@@ -13,6 +13,10 @@ namespace Practice7
         {
             this.costCalculationStrategy = costCalculationStrategy;
         }
+        public void SetPaymentMethod(ICostCalculationStrategy costCalculationStrategy)
+        {
+            this.costCalculationStrategy = costCalculationStrategy;
+        }
         public double CalculateCost(int countOfPassengers, double distance, bool hasADiscount, string classOfService)
         {
             return costCalculationStrategy.CalculateCost(countOfPassengers, distance, hasADiscount, classOfService);
